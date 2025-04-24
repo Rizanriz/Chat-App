44ab504a7d8a5fc6668fba5212caa3d1d697df9d
Rizanriz <rizanr7777@gmail.com>
2025-04-24 17:37:06 +0400

    ongoing

--- backend/index.js
+++ backend/index.js
@@ -25,12 +25,12 @@ app.use("/api/auth",authroutes);
 app.use("/api/messages",messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/build")));
-
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
+  
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
-    })
-}
+      res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    });
+  }
 
 server.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
